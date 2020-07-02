9ad7678a4d9c8b44dfa0155420e75de6cb0d6aab
Vladimir Semenov <sepsseven@gmail.com>
2020-07-02 18:23:32 +0300

    change package

--- server/app.py
+++ server/app.py
@@ -1,23 +1,21 @@
-import flask
 import os
 from flask import Flask, render_template
 
 
-# os.chdir("../frontend")
-# os.system("ng build")
-# os.chdir("..")
-# os.system("cp frontend/dist/miniburn/index.html server/templates/")
-# os.system("cp -a frontend/dist/miniburn/ server/static/")
-# os.chdir("server")
-# check commit stage
-# check 2
+
+os.chdir("../frontend")
+os.system("ng build")
+os.chdir("..")
+os.system("cp frontend/dist/miniburn/index.html server/templates/")
+os.system("cp -a frontend/dist/miniburn/ server/static/")
+os.chdir("server")
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-		return render_template('index.html')
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
