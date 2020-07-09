ea4a6393125894aadcbfea34a42eb120e09251e0
Vladimir Semenov <ext.vsemenov@tinkoff.ru>
2020-07-09 22:24:35 +0300

    update formating

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,11 +1,17 @@
-import os
-import json
-from bson.objectid import ObjectId
+"""
+Miniburn API
 
-import flask
-from flask import Flask, request, jsonify
-from flask_mongoengine import MongoEngine
+Author: Vladimir Semenov
+"""
+
+import os
+
+from exceptions.exceptions import handle_exceptions
+
+from flask import Flask, request
 from flask_cors import CORS
+
+from dotenv import load_dotenv
 
 from database.models.project import Project
 from database.models.task import Task
@@ -14,9 +20,6 @@ from database.db import initialize_db
 from business.crud import create, update, read, delete
 from business.helpers.task_interpreter import get_id
 
-from exceptions.exceptions import handle_exceptions
-
-from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -24,9 +27,9 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}, send_wildcard=True)
 
 app.config['MONGODB_SETTINGS'] = {
-  'db': os.getenv("MONGO_DB"),
-  'host': os.getenv("MONGO_HOST"),
-  'port': int(os.getenv("MONGO_PORT"))
+    'db': os.getenv("MONGO_DB"),
+    'host': os.getenv("MONGO_HOST"),
+    'port': int(os.getenv("MONGO_PORT"))
 }
 
 initialize_db(app)
@@ -34,102 +37,105 @@ initialize_db(app)
 
 @app.route('/')
 def hello_world():
-		return 'Welcome'
+    """Standard response"""
+    return 'Welcome'
 
 
 @app.before_request
-def request_filter():
-  if request.method == "OPTIONS":
-    pass
-  else:
-    print(request.headers)
-    print(request.data)
-    print(request.get_json(force=False, silent=True, cache=True))
-    errors = handle_exceptions(request)
+def request_filter(): # pylint: disable=R1710
+    """Check if request is valid"""
+    if request.method != "OPTIONS":
+        errors = handle_exceptions(request)
 
-    if errors:
-      return errors
-
-# @app.before_request
-# def print_request():
-#   pass
+        if errors:
+            return errors
 
 
 # READ
 @app.route('/api/projects/read', methods=["POST"])
 def get_projects():
-  return read(Project,  request.get_json()), 200
+    """Read project by filter"""
+    return read(Project, request.get_json()), 200
 
 
 @app.route('/api/tasks/read', methods=["POST"])
 def get_tasks():
-  res = read(Task, request.get_json())
-  print(res)
-  return read(Task, request.get_json()), 200
+    """Read tasks by filter"""
+    res = read(Task, request.get_json())
+    print(res)
+    return read(Task, request.get_json()), 200
 
 
 # CREATE
 @app.route('/api/projects/create', methods=["POST"])
 def create_project():
-  data = request.get_json()
-  if "isStarted" not in data.keys():
-    data["isStarted"] = False
-  if "isFinished" not in data.keys():
-    data["isFinished"] = False
-  create(Project, request.get_json())
+    """Create new project with passed parameters"""
+    data = request.get_json()
+    if "isStarted" not in data.keys():
+        data["isStarted"] = False
+    if "isFinished" not in data.keys():
+        data["isFinished"] = False
+    create(Project, request.get_json())
 
-  return "OK"
+    return "OK"
 
 
 @app.route('/api/tasks/create', methods=["POST"])
 def create_task():
-  new_id = create(Task, request.get_json())
+    """Create new task with passed parameters"""
+    new_id = create(Task, request.get_json())
 
-  return new_id, 200
+    return new_id, 200
 
 
 # UPDATE
 @app.route('/api/projects/update', methods=["POST"])
 def update_project():
-  data = request.get_json()
-  update(Task, data, {"_id": get_id(data)})
+    """Update project with passed parameters"""
+    data = request.get_json()
+    update(Task, data, {"_id": get_id(data)})
 
-  return "OK"
+    return "OK"
 
 
 @app.route('/api/tasks/update', methods=["POST"])
 def update_task():
-  data = request.get_json()
-  update(Task, data, {"_id": get_id(data)})
+    """Update task with passed parameters"""
+    data = request.get_json()
+    update(Task, data, {"_id": get_id(data)})
 
-  return "OK"
+    return "OK"
 
 
 # DELETE
 @app.route('/api/projects/delete', methods=["POST"])
 def delete_project():
-  data = request.get_json()
-  delete(Project, {"_id": get_id(data)})
+    """Delete project by id"""
+    data = request.get_json()
+    delete(Project, {"_id": get_id(data)})
 
-  return "OK"
+    return "OK"
 
 
 @app.route('/api/tasks/delete', methods=["POST"])
 def delete_task():
-  data = request.get_json()
-  delete(Task, {"_id": get_id(data)})
+    """Delete task by id"""
+    data = request.get_json()
+    delete(Task, {"_id": get_id(data)})
 
-  return "OK"
+    return "OK"
 
 
 @app.after_request
 def add_access_headers(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Methods', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  # response.h eaders.add('Access-Control-Allow-Credentials', 'true')
-  return response
+    """Add CORS headers to each response"""
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', '*')
+    response.headers.add(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept')
+    return response
 
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    app.run(debug=True)
