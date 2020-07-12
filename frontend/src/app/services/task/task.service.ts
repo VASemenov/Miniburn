350fee899ad02b8e747abd32fbe72448f67acbed
Vladimir Semenov <ext.vsemenov@tinkoff.ru>
2020-07-12 17:33:21 +0300

    remove console logs

--- frontend/src/app/services/task/task.service.ts
+++ frontend/src/app/services/task/task.service.ts
@@ -1,64 +1,58 @@
-import { Injectable, Inject } from "@angular/core";
-import { API } from "../api/api.service"
+import { Injectable, Inject } from '@angular/core';
+import { API } from '../api/api.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { APIModes } from '../api/enums/modes.enum';
 import { Task } from '../../models/task.model';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
-  constructor(
-    @Inject(HttpClient) private http: HttpClient,
-    private api: API
-  ) {
-  }
+  constructor(@Inject(HttpClient) private http: HttpClient, private api: API) {}
 
   public create(task: Task) {
-    return this.http.post(this.api.TASKS(APIModes.CREATE), task)
+    return this.http.post(this.api.TASKS(APIModes.CREATE), task);
   }
 
   public read(id: string) {
-    let body = {
-      "_id": id
-    }
+    const body = {
+      _id: id,
+    };
 
-    return this.http.post(this.api.TASKS(APIModes.READ), body)
+    return this.http.post(this.api.TASKS(APIModes.READ), body);
   }
 
-  public filter(filter: Object) {
+  public filter(filter: object) {
     return this.http.post<any>(this.api.TASKS(APIModes.READ), filter);
   }
 
-  public update(id:string, changes: Object) {
-    console.log(id)
-    let body = {
-      "_id": id,
-      ...changes
-    }
-    console.log("REQUEST")
-    console.log(body)
-    return this.http.post(this.api.TASKS(APIModes.UPDATE), body, {responseType: 'text'})
+  public update(id: string, changes: object) {
+    const body = {
+      _id: id,
+      ...changes,
+    };
+    return this.http.post(this.api.TASKS(APIModes.UPDATE), body, {
+      responseType: 'text',
+    });
   }
 
   public delete(id: string) {
-    let body = {
-      "_id": id
-    }
+    const body = {
+      _id: id,
+    };
 
-    return this.http.post(this.api.TASKS(APIModes.DELETE), body, {responseType: 'text'})
+    return this.http.post(this.api.TASKS(APIModes.DELETE), body, {
+      responseType: 'text',
+    });
   }
 
-  private pack(body: Object) {
+  private pack(body: object) {
     return JSON.stringify(body);
   }
 
   private getHeaders(): HttpHeaders {
-    let headers = new HttpHeaders()
-    headers.set('Content-Type', 'application/json; charset=utf-8')
-    return headers
+    const headers = new HttpHeaders();
+    headers.set('Content-Type', 'application/json; charset=utf-8');
+    return headers;
   }
-
 }
-
